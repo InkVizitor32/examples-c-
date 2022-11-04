442cd4a5253ad5df8f340e0150bf4f5fc411f6aa
InkVizitor32 <morgunov.roman89@gmail.com>
2022-11-04 17:40:59 +0300

    создали папку и написали программу приветствия

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Если WriteLINE, то будет построчно
+// еСли Write то в одной строке
+
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+Console.Write("!");
